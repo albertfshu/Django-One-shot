fd34d7cc9093ec6914dd75940e2b78cdfdcbf8ef
Albert Shu <albert94shu@gmail.com>
2023-04-18 11:45:13 -0500

    Feature 7 Complete

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from todos.models import TodoList
+
 
 # Create your views here.
+def todo_list(request):
+    todo_lists = TodoList.objects.all()
+    context = {
+        "todo_list": todo_lists,
+    }
+    return render(request, "todos/todo_list.html", context)
