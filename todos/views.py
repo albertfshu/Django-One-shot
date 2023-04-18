1f94f3caba162253c2525c596bccf3b7b4684574
Albert Shu <albert94shu@gmail.com>
2023-04-18 14:39:22 -0500

    Feature 8  Is So Hard

--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from todos.models import TodoList
+from django.shortcuts import render, get_object_or_404, redirect
+from todos.models import TodoList, TodoItem
 
 
 # Create your views here.
@@ -9,3 +9,25 @@ def todo_list(request):
         "todo_list": todo_lists,
     }
     return render(request, "todos/todo_list.html", context)
+
+
+def todo_list_detail(request, id):
+    todolist = get_object_or_404(TodoList, id=id)
+    context = {"todolist": todolist}
+    return render(request, "todos/todo_list_detail.html", context)
+
+
+def create_todo_list(request, id):
+    if request.method == "POST":
+        form = TodoListForm(request.POST)
+        if form.is_valid():
+            todo_list = form.save()
+            return redirect(
+                request,
+                "todo_list_detail",
+            )
+    else:
+        form = TodoListForm()
+
+    context = {"form": form}
+    return render(request, "todo_list/create_todo_list.html", context)
