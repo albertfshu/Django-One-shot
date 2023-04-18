dcbf88dca9d5d40ea19351a49b5e0d1179ad544f
Albert Shu <albert94shu@gmail.com>
2023-04-18 10:23:09 -0500

    Feature 3 Complete

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class TodoList(models.Model):
+    name = models.CharField(max_length=100)
+    created_on = models.DateTimeField(default=timezone.now)
